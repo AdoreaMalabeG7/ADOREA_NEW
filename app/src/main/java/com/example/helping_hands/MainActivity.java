e06d160f03b4aab9e3a86baaefb46d4cd6198822
IT20275792 <uthsarakawmini@gmail.com>
2021-09-18 16:33:19 +0530

    MAIN

---
+++ app/src/main/java/com/example/helping_hands/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helping_hands;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

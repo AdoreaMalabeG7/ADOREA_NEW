7a8d06107bd610cc5035c38c6cfe68f92a7dee08
Osanda Randunu <it20082888@my.sliit.lk>
2021-09-22 16:10:25 +0530

    main page of establishment

---
+++ app/src/main/java/com/example/HELPING_HANDS/Activity_establishment.java
@@ -0,0 +1,14 @@
+package com.example.HELPING_HANDS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_establishment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_establishment);
+    }
+}

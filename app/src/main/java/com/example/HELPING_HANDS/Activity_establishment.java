5b19f2a236f06e2d5924cbdde337f04c42a11f58
Osanda Randunu <it20082888@my.sliit.lk>
2021-09-24 04:25:05 +0530

    all the UIs except one

--- app/src/main/java/com/example/HELPING_HANDS/Activity_establishment.java
+++ app/src/main/java/com/example/HELPING_HANDS/Activity_establishment.java
@@ -2,13 +2,59 @@ package com.example.HELPING_HANDS;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_establishment extends AppCompatActivity {
+    private Button button10;
+    private Button button11;
+    private Button button12;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_establishment);
+
+        button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistration();
+            }
+        });
+
+        button11 = (Button) findViewById(R.id.button11);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDhanares();
+            }
+        });
+        button12 = (Button) findViewById(R.id.button12);
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegistered();
+            }
+        });
+    }
+
+
+    public void openRegistration(){
+        Intent intent = new Intent(this, HomeRegistration.class);
+        startActivity(intent);
+    }
+
+    public void openDhanares(){
+        Intent intent1 = new Intent(this, mealType.class);
+        startActivity(intent1);
+    }
+
+    public void openRegistered(){
+        Intent intent2 = new Intent(this, registeredHomes.class);
+        startActivity(intent2);
     }
 }
---
+++ app/src/main/java/com/example/HELPING_HANDS/mealType.java
@@ -0,0 +1,14 @@
+package com.example.HELPING_HANDS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mealType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_type);
+    }
+}

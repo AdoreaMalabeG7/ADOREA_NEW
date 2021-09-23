bf2fe4947e2705aaa2775ea7a728d780a78cbf25
IT20275792 <uthsarakawmini@gmail.com>
2021-09-23 17:10:56 +0530

    Success DB connection

--- app/src/main/java/com/example/HELPING_HANDS/Registerbd.java
+++ app/src/main/java/com/example/HELPING_HANDS/Registerbd.java
@@ -6,18 +6,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Registerbd extends AppCompatActivity {
-
+    EditText bdFullname,bdBirth,bdWigth,bdAdrs,bdLdd,bdPhn;
+    CheckBox bdG1,bdG2;
     Button rgs;
+
+    FirebaseDatabase rootnode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registerbd);
 
+        bdFullname = findViewById(R.id.nameT);
+        bdBirth = findViewById(R.id.WigtT);
+        bdG1 = findViewById(R.id.GndM);
+        bdG2 = findViewById(R.id.GndF);
+        bdWigth = findViewById(R.id.WigtT);
+        bdAdrs = findViewById(R.id.addrsT);
+        bdLdd = findViewById(R.id.LddT);
+        bdPhn = findViewById(R.id.PhnT);
         rgs=findViewById(R.id.rg);
+
+
+
+
+        rgs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    String name = bdFullname.getText().toString().trim();
+                    String Bday = bdBirth.getText().toString().trim();
+                    String weight = bdWigth.getText().toString().trim();
+                    String adress = bdAdrs.getText().toString().trim();
+                    String ldd = bdLdd.getText().toString().trim();
+                    String phone = bdPhn.getText().toString().trim();
+
+                rootnode=FirebaseDatabase.getInstance();
+            reference=rootnode.getReference("Register");
+
+//                bdHelperDB helperClass=new bdHelperDB(name,Bday,weight,adress,ldd,phone);
+//            reference.child(phone).setValue(bdHelperDB);
+//
+            }
+        });
+
+
+
+            rgs=findViewById(R.id.rg);
         rgs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
